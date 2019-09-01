f23a438553f2e8fd888acec86345ba2585982b59
Koji Wakamiya <koji.wakamiya@gmail.com>
2019-09-01 09:36:39 +0900

    Update module

--- app/src/main/java/com/dr1009/app/dbsearch/di/AppModule.kt
+++ app/src/main/java/com/dr1009/app/dbsearch/di/AppModule.kt
@@ -3,23 +3,27 @@ package com.dr1009.app.dbsearch.di
 import android.content.Context
 import androidx.room.Room
 import com.dr1009.app.dbsearch.App
+import com.dr1009.app.dbsearch.db.WordDao
 import com.dr1009.app.dbsearch.db.WordDatabase
 import dagger.Module
 import dagger.Provides
 import javax.inject.Singleton
 
 @Module
-internal class AppModule {
+internal object AppModule {
 
+    @JvmStatic
     @Provides
     fun provideContext(app: App): Context = app.applicationContext
 
+    @JvmStatic
     @Singleton
     @Provides
     fun provideDB(context: Context): WordDatabase = Room
             .databaseBuilder(context, WordDatabase::class.java, "DBSearch.db").build()
 
+    @JvmStatic
     @Singleton
     @Provides
-    fun provideWordDao(db: WordDatabase) = db.wordDao()
+    fun provideWordDao(db: WordDatabase): WordDao = db.wordDao()
 }
